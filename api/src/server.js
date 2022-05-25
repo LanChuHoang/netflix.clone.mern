5442c95f332b46a00818e42a263eca0f4ab5e5c4
LanChuHoang <hoanglan.ict@gmail.com>
2022-05-25 20:57:47 +0700

    init & connect mongodb

--- api/src/server.js
+++ api/src/server.js
@@ -1,8 +1,11 @@
 const http = require("http");
 const app = require("./app");
+const mongoService = require("./services/mongo");
 
 const server = http.createServer(app);
 const PORT = 8000;
+
+mongoService.connect();
 
 server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
